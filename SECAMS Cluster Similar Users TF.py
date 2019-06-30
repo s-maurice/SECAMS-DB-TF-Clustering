6cbad8a2d94fe9f6d0b5e9912858be7783bcf229
hYm3nSl4yr <imauriceinoob@gmail.com>
2019-06-30 19:21:03 +0800

    Added DNNBuilder, started defining train_imput_fn, Added Shuffling and split train/test datasets to preprocessing.

--- SECAMS Cluster Similar Users TF.py
+++ SECAMS Cluster Similar Users TF.py
@@ -43,7 +43,7 @@ def dataPreprocessing(df):
     #normedtime x/y
     timexy = dayTimeNormed(df["TIMESTAMPS"])
     df = df.join(timexy)
-    df['DAYTIME'] = df[['daytx', 'dayty']].values.tolist()
+    df['DAYTIME'] = df[['daytx', 'dayty']].values.tolist() #puts daytx and dayty into a single df column, with array dimension (2,1)
     #day of week
     dow = df["TIMESTAMPS"].dt.strftime("%a")
     df["DAYOFWEEK"] = dow
@@ -51,13 +51,26 @@ def dataPreprocessing(df):
     moy = df["TIMESTAMPS"].dt.strftime("%b")
     df["MONTHOFYEAR"] = moy
 
-    print(df[["TIMESTAMPS", "daytx", "dayty", "DAYOFWEEK", "MONTHOFYEAR"]])
-    return df
+    #Shuffle DF Rows
+    df = df.sample(frac=1).reset_index(drop=True)  #could use shuffle(df) from sklearn.utils
+    df_size = len(df.index)
+    #Select ratio of train to test data - NEEDS REMAKE to handle decimal, give train_test_ratio differently - TEMP
+    train_test_ratio = (4, 5)
+
+    train_len = int(df_size * train_test_ratio[0] / train_test_ratio[1])
+    test_len = int(df_size - (df_size * train_test_ratio[0] / train_test_ratio[1]))
+
+    df_train = df.head(train_len)
+    df_test = df.tail(test_len)
+
+    print(train_len, test_len)
+
+    return df_train, df_test
 
     #remember to split into training and test datasets
 
 
-def training_input_func(dataset):
+def define_feature_columns(dataset):
     sparsedata = dataset.drop(["TIMESTAMPS", "daytx", "dayty"], axis=1) #Not nessacary, prep for iterable in future?
 
     #Create Feature Columns with each possible value for sparse data rows
@@ -69,25 +82,39 @@ def training_input_func(dataset):
     #numeric for timescale
     daytime_fc = tf.feature_column.numeric_column(key="DAYTIME", shape=[2,1]) #put both daytx and dayty in as array, double check the shape
 
+    feature_columns_list = [USERID_fc, EVENTID_fc, TERMINALSN_fc, DOW_fc, MOY_fc, daytime_fc]
 
     #Wrap within an embedding column
     #Sparse feature columns
-    USERID_em = tf.feature_column.indicator_column(categorical_column=USERID_fc, dimension=3) #dimension should be about number_of_categories**0.25 according to google
-    EVENTID_em = tf.feature_column.indicator_column(categorical_column=EVENTID_fc, dimension=2) #indicator or embedding column???
-    TERMINALSN_em = tf.feature_column.indicator_column(categorical_column=TERMINALSN_fc, dimension=50)
-    DOW_em = tf.feature_column.indicator_column(categorical_column=DOW_fc, dimension=1)
-    MOY_em = tf.feature_column.indicator_column(categorical_column=MOY_fc, dimension=2)
-    #dense feature columns - need to embed?
+    # USERID_em = tf.feature_column.indicator_column(categorical_column=USERID_fc) #dimension should be about number_of_categories**0.25 according to google, for embedding columns
+    # EVENTID_em = tf.feature_column.indicator_column(categorical_column=EVENTID_fc) #indicator or embedding column???
+    # TERMINALSN_em = tf.feature_column.indicator_column(categorical_column=TERMINALSN_fc)
+    # DOW_em = tf.feature_column.indicator_column(categorical_column=DOW_fc)
+    # MOY_em = tf.feature_column.indicator_column(categorical_column=MOY_fc)
+    # #dense feature columns - need to embed?
 
-    #tf.estimator.DNNRegressor()
+    return feature_columns_list
+
+def DNNBuilder(fc_list):
+
+    #Build DNN Classifier - #USE DNNRegressor or DNNClassifier
+    Classifier = tf.estimator.DNNClassifier(feature_columns=fc_list, hidden_units=[256, 32]) #Not sure how many hidden units, layers/size, need more research/expermentation
+
+    return Classifier
+
+def train_input_fn(df):
+    input_fn = tf.estimator.inputs.pandas_input_fn(df, shuffle=True) #other params needed?
+
 
 #CategoricalColumn
 
 def main():
     eventDF = getEvents()
-    eventDF = dataPreprocessing(eventDF)
-    training_input_func(eventDF)
-    print("ML Begin")
+    df_train, df_test = dataPreprocessing(eventDF)
+    fc_list = define_feature_columns(df_train)
+    print("DNNBuilder")
+    Classifier = DNNBuilder(fc_list)
+    train_input_fn(df_train)
 
 
 

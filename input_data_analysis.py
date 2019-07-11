d664ea79028ea5ffa1c163951829810d61c95452
Jonathan Yim <canpake@users.noreply.github.com>
2019-07-11 16:28:55 +0800

    revamped show_pattern to take in a list of user IDs and display them together

--- input_data_analysis.py
+++ input_data_analysis.py
@@ -17,29 +17,50 @@ def show_usercounts():
     plt.show()
 
 
-def show_pattern(userid):
-    # Shows a graph of the event patterns of a particular user by time and event
-    csv_filename = "entries_by_user/SECAMS_user_" + str(userid) + ".csv"
-    #csv_filename = "Curated Data/ALL_USERID_begining_with_20_and_between_100_and_500_entries.csv"
-    usercount_df = pd.read_csv(csv_filename)
-    print(usercount_df)
-    usercount_df['TIMESTAMPS'] = pd.to_datetime(usercount_df['TIMESTAMPS'])
-
+def show_pattern(userid_list):
+    # Shows a graph of the event patterns of a set of users by time and event
     # Create the plot
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(nrows=len(userid_list), sharex=True)
 
-    # Set x and y
-    ax.scatter(x=usercount_df['TIMESTAMPS'],
-               y=usercount_df['EVENTID'])
+    fig.subplots_adjust(bottom=0.2, hspace=0.4)
 
-    # Rotate tick labels
-    plt.setp(ax.get_xticklabels(), rotation=45)
+    # Check if list contains single or multiple users (otherwise 'ax' will be an object instead of an array)
+    if len(userid_list) == 1:
+        # set title and labels for axes
+        ax.set(xlabel="Date",
+               ylabel="Event ID",
+               title="Event logs for user " + str(userid_list[0]));
 
-    # set title and labels for axes
-    ax.set(xlabel="Date",
-           ylabel="Event ID",
-           title="Event logs for user " + str(userid));
+        # Rotate tick labels
+        plt.setp(ax.get_xticklabels(), rotation=45)
+
+        # Get data from CSV, then set x and y
+        csv_filename = "CSV Files/entries_by_user/SECAMS_user_" + str(userid_list[0]) + ".csv"
+        usercount_df = pd.read_csv(csv_filename)
+        usercount_df['TIMESTAMPS'] = pd.to_datetime(usercount_df['TIMESTAMPS'])
+
+        ax.scatter(x=usercount_df['TIMESTAMPS'],
+                   y=usercount_df['EVENTID'])
+
+    else:
+    # Graph each user
+        for userid, i in zip(userid_list, range(len(userid_list))):
+            # set title and labels for axes
+            ax[i].set(xlabel="Date",
+                      ylabel="Event ID",
+                      title="Event logs for user " + str(userid_list[i]));
+
+            # Rotate tick labels
+            plt.setp(ax[i].get_xticklabels(), rotation=45)
+
+            # Get data from CSV, then set x and y
+            csv_filename = "entries_by_user/SECAMS_user_" + str(userid) + ".csv"
+            usercount_df = pd.read_csv(csv_filename)
+            usercount_df['TIMESTAMPS'] = pd.to_datetime(usercount_df['TIMESTAMPS'])
+
+            ax[i].scatter(x=usercount_df['TIMESTAMPS'],
+                          y=usercount_df['EVENTID'])
 
 
-show_pattern("Unknown_User")
+show_pattern([20018])
 plt.show()

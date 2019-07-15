31b49607cb1163370b4851d9f89564895aba998e
hYm3nSl4yr <imauriceinoob@gmail.com>
2019-07-15 17:15:20 +0800

    made new generator, but this one wont be used properly

--- data_generator/schedule_gen.py
+++ data_generator/schedule_gen.py
@@ -20,7 +20,7 @@ import datetime
 #   L, lunch (e.g. a canteen)
 #   M, meeting room
 #   0, away from school (home)
-def generate_daily_schedule(total_rooms=20,
+def generate_daily_schedule(total_rooms=15,
                             periods=8,
                             break_rooms=2,
                             lunch_period_position=3,
@@ -66,6 +66,50 @@ def generate_daily_schedule(total_rooms=20,
     return schedule_df
 
 
+# New generator
+def generator_by_period_remove_classrooms_from_list(lunch_period=3):
+    full_time_df = pd.DataFrame()
+    part_time_df = pd.DataFrame()
+    for i in range(6+1):  # Periods in day, plus one for lunch
+        avaliable_classrooms = [("C"+str(i)) for i in range(15)]  # Reset list of avaliable rooms
+        used_classrooms = []
+        for ii in range(10):  # num of full time teachers
+            if i == lunch_period:
+                current_room = "L"
+            else:
+                # Get and remove the current classroom from available classroom list
+                current_room_index = np.random.randint(len(avaliable_classrooms))
+                current_room = avaliable_classrooms[current_room_index]
+                used_classrooms.append(current_room)
+                avaliable_classrooms.pop(current_room_index)
+            full_time_df.loc["period"+str(i), "full_time"+str(ii)] = current_room  # Add classroom to Data Frame
+        for iii in range(5):  # num of part time teachers
+            if i == lunch_period:
+                current_room = "L"
+            else:
+                # Adds in bias so that part timers prefer their own classrooms
+                # this should be implemented elsewhere, currently has no effect
+                current_part_time_bias_list_index = np.random.randint(0, len(avaliable_classrooms), size=2) # biased towards 3 particular classrooms
+                current_part_time_bias_list = avaliable_classrooms + 2*[avaliable_classrooms[i] for i in current_part_time_bias_list_index]  # how heavily it's biased
+
+                # Adds in some used classrooms to have teachers double up, biases can be changed as above
+                used_classrooms_bias_list_index = np.random.randint(0, len(used_classrooms), size=2)
+                used_classrooms_bias_list = [used_classrooms[i] for i in used_classrooms_bias_list_index]
+                current_part_time_bias_list += used_classrooms_bias_list  # These two lines can be combined into one
+
+                current_room_index = np.random.randint(len(avaliable_classrooms))
+                current_room = current_part_time_bias_list[current_room_index]
+            part_time_df.loc["period" + str(i), "part_time" + str(iii)] = current_room  # Add classroom to Data Frame
+    # Chop out part time morning/afternoons here
+
+
+    print(full_time_df)
+    print(part_time_df)
+
+
+
+
+
 # Each user has the same daily schedule, repeated across weekdays, with the exception of a weekly meeting on Wednesday.
 # Turns daily schedule df into a list of DataFrames,
 # with each dataframe representing a single user's weekly schedule,
@@ -98,8 +142,11 @@ def generate_timestamps(user_list):
     print(a)
     print(user_list[0])
 
+#
+# daily_schedule = generate_daily_schedule()
+# print(daily_schedule)
+# user_list = generate_user_weekly_schedules(daily_schedule, after_school_meeting=True)
+# print(user_list[1])
+#generate_timestamps(user_list)
 
-daily_schedule = generate_daily_schedule()
-#print(daily_schedule)
-user_list = generate_user_weekly_schedules(daily_schedule, after_school_meeting=True)
-generate_timestamps(user_list)
+generator_by_period_remove_classrooms_from_list()

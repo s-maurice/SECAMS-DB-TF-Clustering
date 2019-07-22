3169732fc8ae6a4019e93582a29675a1a6bcf450
Jonathan Yim <canpake@users.noreply.github.com>
2019-07-22 16:24:55 +0800

    attendance_gen gives reasons; uploaded the result DF

--- absence_detect/attendance_generator.py
+++ absence_detect/attendance_generator.py
@@ -4,7 +4,7 @@ import datetime as dt
 import time
 
 
-# Defines an event based on two booleans
+# Defines an event based on two booleans; idx is so the method can be applied to a DataFrame
 def get_event(present, weekday):
     # Normal: present on weekdays, absent on weekends
     if present == weekday:
@@ -17,23 +17,31 @@ def get_event(present, weekday):
         return "Absent"
 
 
-# Defines an event based on two booleans
-def get_event_alt(x):
-    present = x[0]
-    weekday = x[1]
-    # Normal: present on weekdays, absent on weekends
-    if present == weekday:
-        return "Normal"
-    # Present on a weekend
-    elif present:
-        return "Weekend"
-    # Absent on a weekday
-    elif not present:
-        return "Absent"
-
+# Obtains the reason for a particular event; day in datetime // event as String
+# In particular:
+#   Absent on a Monday - Hungover
+#   Absent on a Friday - Skiving
+#   Absent on other days - Car broke down / Sick / Other activities
+#   Absent on specific days - Holidays
+def get_reason(day, event):
+    if event == "Absent":
+        if day.weekday() == 0:
+            return "Hungover"
+        elif day.weekday() == 4:
+            return "Skiving"
+        else:
+            return "Sick"
+    else:
+        return event
 
 df = pd.read_csv("absence_df.csv")
+df['Day'] = pd.to_datetime(df['Day'])
 df.set_index("index", inplace=True)
-df['Event'] = df[['Present', 'Weekday']].apply(get_event, args=(1,), axis=1)
+df['Event'] = [get_event(present, weekday) for present, weekday in zip(df['Present'], df['Weekday'])]
+df['Reason'] = [get_reason(day, event) for day, event in zip(df['Day'], df['Event'])]
 
 print(df)
+
+df.to_csv("reason_df.csv")
+
+print('dun')

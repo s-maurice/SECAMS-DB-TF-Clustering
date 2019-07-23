58cfa8e990a85f902f091305087748098164ebac
Jonathan Yim <canpake@users.noreply.github.com>
2019-07-23 16:54:24 +0800

    test_result_plotter now plots samples from the test dataset
    (todo: change to plot columns and rows; plots feature information as well alongside each graph)

--- absence_detect/absence_classifier.py
+++ absence_detect/absence_classifier.py
@@ -75,6 +75,7 @@ def train_model(train_features, train_targets, test_features, test_targets, lear
     feature_columns = create_feature_columns(train_features)
     label_vocab_list = train_targets['Reason'].unique().tolist()
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    # optimizer = tf.contrib.estimator.clip_gradients_by_norm(optimizer, 5.0)
 
     # Create the DNN
     classifier = tf.estimator.LinearClassifier(feature_columns=feature_columns,
@@ -97,15 +98,10 @@ def train_model(train_features, train_targets, test_features, test_targets, lear
 
     for period in range(periods):
         classifier.train(input_fn=train_fn, steps=steps_per_period)
-        print('train')
 
-        classifier.evaluate(predict_train_fn, name="Train")
-        print('eval 1')
-
-        classifier.evaluate(predict_test_fn, name="Test")
-        print('eval 2')
-
-        print('period ' + str(period) + ' finished')
+        print('Period ' + str(period) + ':')
+        print("Training:", classifier.evaluate(predict_train_fn, name="Train"))
+        print("Testing:", classifier.evaluate(predict_test_fn, name="Test"))
 
     print("Training ended.")
 
@@ -113,28 +109,116 @@ def train_model(train_features, train_targets, test_features, test_targets, lear
 
 
 def predict_model(classifier, features, targets):
-    predict_input_fn = create_input_function(features, targets, batch_size=1, num_epochs=1)
+    # A method that takes in a model, and returns a generator that yields predictions
+    # Note: Each iteration through predict_results returns a dict with the following keys:
+    #  logits
+    #  probabilities    (use this)
+    #  class_ids
+    #  classes
+    #  all_class_ids
+    #  all_classes      (use this too)
+    predict_input_fn = lambda: create_input_function(features, targets, batch_size=1, num_epochs=1)
 
-    predict_results = classifier.predict(predict_input_fn, predict_keys="probabilities")
+    predict_results = classifier.predict(predict_input_fn)
 
-    print(predict_results.get("probabilities"))
+    i = 0
+    for idx, prediction in enumerate(predict_results):
+        if i == 10:
+            break
+        for key, value in prediction.items():
+            print(key, ":", value)
+        i += 1
+
     return predict_results
 
 
-def test_result_plotter(result_df, num):
-    result_df = result_df.sample(frac=1).reset_index(drop=True)
-    results_to_plot = result_df.head(num)
-    # print(results_to_plot)
+def test_result_plotter(predict_results, num_results):
+    # A method that takes in a generator of predictions, and plots the first few (num_results) onto a PLT figure.
+    # --- Getting a Dict from predict_results (as label -> list of probabilities)
+    result_dict = {}
 
-    # Get highest probability predicted and lock all figures's y axis to that
-    max_value = results_to_plot.iloc[[0, -1]].max()
-    max_value = max_value[0:-1].max()
+    # Iterate over predict_results and enter the first predictions in to the dataframe
+    for idx, prediction in enumerate(predict_results):
+        # break if the index is equal to number
+        if idx == num_results:
+            break
 
-    fig, axes = plt.subplots(nrows=num, ncols=1, sharex=True)
-    for index, row in results_to_plot.iterrows():
-        plot_row(row, axes[index], results_to_plot.columns[0:-1], max_value, show_actual_label=True)
+        # Get the list of probabilities, list of classes, and actual class
+        probabilities = prediction.get("probabilities")
+        classes = [label.decode() for label in prediction.get("all_classes")]
+        actual_class = prediction.get("class_ids")[0]  # [0] because .get("classes") returns an array of 1 item
+
+        # Enumerate through classes; add and update the key-value pairs
+        for i, label in enumerate(classes):
+            # If label exists, update the list
+            if label in result_dict.keys():
+                result_dict[label].append(probabilities[i])
+            else:
+                result_dict[label] = [probabilities[i]]
+
+        # Add/update an additional key-value pair: Actual class
+        if "actual_class" in result_dict.keys():
+            result_dict["actual_class"].append(actual_class)
+        else:
+            result_dict["actual_class"] = [actual_class]
+
+    print(result_dict)
+
+    # --- Plotting result_dict
+    # Make a figure with axes
+    fig, axes = plt.subplots(nrows=num_results, ncols=1, sharex=True)
     fig.canvas.set_window_title('Testing Results')
     fig.suptitle("Test Predict Result Percentages")
+
+    # Plot each row
+    for row in range(num_results):
+
+        row_results = [result_dict.get(label)[row] for label in result_dict.keys()]
+        row_labels = list(result_dict.keys())
+
+        # Ignore last entries, the 'actual_labels'
+        row_results = row_results[:-1]
+        row_labels = row_labels[:-1]
+
+        print(row_results)
+        print(row_labels)
+
+        # Plot results
+        bars = axes[row].bar(x=row_labels, height=row_results, color="gray")
+
+        # Find the actual and predicted labels; change their colours
+        predict_label = np.argmax(row_results)
+        actual_label = result_dict.get("actual_class")[row]
+
+        bars[predict_label].set_color('r')    # Predict overrides the gray
+        bars[actual_label].set_color('b')     # If actual == predict, then this overrides the previous statement
+
+        # Draw the mean as a horizontal line
+        mean = sum(row_results) / len(row_results)
+        axes[row].axhline(mean, color='k', linestyle='dashed', linewidth=1)
+
+
+
+    # result_df = result_df.sample(frac=1).reset_index(drop=True)
+    # results_to_plot = result_df.head(num)
+    # # print(results_to_plot)
+    #
+    # # Get highest probability predicted and lock all figures's y axis to that
+    # max_value = results_to_plot.iloc[[0, -1]].max()
+    # max_value = max_value[0:-1].max()
+    #
+
+    #
+    # # Plot each result
+    # for index, row in results_to_plot.iterrows():
+    #     # Find the predicted and actual labels
+    #     # predict_label = np.argmax(row[:-1].values)
+    #     # actual_label = xlabels.index(row.values[-1])
+
+    #
+    #     # plot_row(row, axes[index], results_to_plot.columns[0:-1], max_value, show_actual_label=True)
+
+    plt.show()
 
 
 def main():
@@ -148,8 +232,6 @@ def main():
 
     raw_df = pd.read_csv("reason_df.csv")
     raw_df['Day'] = pd.to_datetime(raw_df['Day'])
-
-    print(raw_df.head(100))
 
     raw_df = raw_df.reindex(np.random.permutation(raw_df.index))
     raw_df.reset_index(inplace=True)
@@ -176,9 +258,8 @@ def main():
                              batch_size=50,
                              model_dir=model_dir_path)
 
-    predict_model(classifier, test_targets, test_targets)
-
-
+    predictions = predict_model(classifier, test_features, test_targets)
+    test_result_plotter(predictions, 4)
 
 
 

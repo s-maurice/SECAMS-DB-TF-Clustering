d35b877029c512b2bf33d9287327c2125d1a5dce
hYm3nSl4yr <imauriceinoob@gmail.com>
2019-08-06 12:29:41 +0800

    changed params for outlier detection

--- SECAMS Clustering Events SciKit Learn.py
+++ SECAMS Clustering Events SciKit Learn.py
@@ -8,27 +8,25 @@ pd.set_option('display.max_columns', 10)
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.width', None)
 
-# Only 14 users, 4000ish lines in this csv
-raw_df = pd.read_csv("CSV Files/Curated Data/ALL_USERID_beginning_with_20_and_between_100_and_500_entries.csv")
+raw_df = pd.read_csv("CSV Files/SECAMS_common_user_id_big.csv")
 raw_df['TIMESTAMPS'] = pd.to_datetime(raw_df['TIMESTAMPS'])
-
 
 # Process Features
 preprocessed_features = pd.DataFrame()
-preprocessed_features["UserID"] = raw_df["USERID"]
-preprocessed_features["TerminalSN"] = raw_df["TERMINALSN"]
+# preprocessed_features["UserID"] = raw_df["USERID"]
+# preprocessed_features["TerminalSN"] = raw_df["TERMINALSN"]
 preprocessed_features["Time_Of_Day"] = [timestamp.hour + timestamp.minute / 60 for timestamp in raw_df["TIMESTAMPS"]]
-preprocessed_features["Day_Of_Month"] = [timestamp.day for timestamp in raw_df["TIMESTAMPS"]]
-preprocessed_features["Day_Of_Week"] = [timestamp.weekday() for timestamp in raw_df["TIMESTAMPS"]]
+# preprocessed_features["Day_Of_Month"] = [timestamp.day for timestamp in raw_df["TIMESTAMPS"]]
+# preprocessed_features["Day_Of_Week"] = [timestamp.weekday() for timestamp in raw_df["TIMESTAMPS"]]
 # preprocessed_features["EventID"] = raw_df["EVENTID"]
 
 
-# Label Encode Features
-UserID_le = preprocessing.LabelEncoder()
-preprocessed_features["UserID"] = UserID_le.fit_transform(preprocessed_features["UserID"])
+# # Label Encode Features
+# UserID_le = preprocessing.LabelEncoder()
+# preprocessed_features["UserID"] = UserID_le.fit_transform(preprocessed_features["UserID"])
 
-TerminalSN_le = preprocessing.LabelEncoder()
-preprocessed_features["TerminalSN"] = TerminalSN_le.fit_transform(preprocessed_features["TerminalSN"])
+# TerminalSN_le = preprocessing.LabelEncoder()
+# preprocessed_features["TerminalSN"] = TerminalSN_le.fit_transform(preprocessed_features["TerminalSN"])
 
 # EventID_le = preprocessing.LabelEncoder()
 # preprocessed_features["EventID"] = EventID_le.fit_transform(preprocessed_features["EventID"])
@@ -38,13 +36,30 @@ preprocessed_features["TerminalSN"] = TerminalSN_le.fit_transform(preprocessed_f
 # train_features, test_features = train_test_split(preprocessed_features, test_size=0.2)
 
 # Begin Training
-neigh = LocalOutlierFactor(novelty=False)  # Default Args
+neigh = LocalOutlierFactor(n_neighbors=300, leaf_size=100, novelty=False, algorithm="auto", contamination=0.01)
 train_outliers = neigh.fit_predict(preprocessed_features)  # On training data
 
+# Compile into Data Frame for print
 outlier_result_df = pd.DataFrame()
-outlier_result_df["UserID"] = UserID_le.inverse_transform(preprocessed_features["UserID"])
-outlier_result_df["TerminalSN"] = TerminalSN_le.inverse_transform(preprocessed_features["TerminalSN"])
-outlier_result_df["Timestamps"] = raw_df["TIMESTAMPS"]
+# outlier_result_df["UserID"] = UserID_le.inverse_transform(preprocessed_features["UserID"])
+# outlier_result_df["TerminalSN"] = TerminalSN_le.inverse_transform(preprocessed_features["TerminalSN"])
+# outlier_result_df["Timestamps"] = raw_df["TIMESTAMPS"]
+outlier_result_df["Time_Of_Day"] = preprocessed_features["Time_Of_Day"]
 outlier_result_df["Outlier"] = train_outliers
 print(outlier_result_df)
+
+# Get Percentage of Outliers
+outlier_percentage = len(outlier_result_df.loc[outlier_result_df["Outlier"] == -1]) / len(outlier_result_df)
+print(outlier_percentage)
+
+# # Outliers in red
+# plt.scatter(outlier_result_df.loc[outlier_result_df["Outlier"] == -1]["Time_Of_Day"], outlier_result_df.loc[outlier_result_df["Outlier"] == -1]["TerminalSN"], c='red', alpha=0.5)
+# # Normals
+# plt.scatter(outlier_result_df.loc[outlier_result_df["Outlier"] == 1]["Time_Of_Day"], outlier_result_df.loc[outlier_result_df["Outlier"] == 1]["TerminalSN"], c='blue', alpha=0.5)
+
+# Outliers in red
+plt.scatter(outlier_result_df.loc[outlier_result_df["Outlier"] == -1]["Time_Of_Day"], [0] * len(outlier_result_df.loc[outlier_result_df["Outlier"] == -1]), c='red', alpha=0.01)
+# Normals
+plt.scatter(outlier_result_df.loc[outlier_result_df["Outlier"] == 1]["Time_Of_Day"], [0] * len(outlier_result_df.loc[outlier_result_df["Outlier"] == 1]), c='blue', alpha=0.01)
+plt.show()
 
